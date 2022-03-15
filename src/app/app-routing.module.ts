64a48352e48fe1eb2ea52fcf5174a0176df7aaa1
LAPTOP-IP5FO39M\KhushiAgrawal <yashagrawal1267@gmail.com>
2022-03-15 17:50:07 +0530

    Exercise 2 commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-
-const routes: Routes = [];
+import { CustomersListComponent } from "./customers-list/customers-list.component";
+const routes: Routes = [
+  {
+    component:CustomersListComponent,
+    path:'customers-list'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { HttpClientModule } from "@angular/common/http";
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NewHttpsComponent } from './new-https/new-https.component';
+import { CustomersListComponent } from './customers-list/customers-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NewHttpsComponent
+    NewHttpsComponent,
+    CustomersListComponent
   ],
   imports: [
     BrowserModule,
